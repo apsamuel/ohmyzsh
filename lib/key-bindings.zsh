1f6366148bee20edf13694e0456a86d556bc10d7
Sven Lito <me@svenlito.com>
2010-10-12 20:02:04 +0100

    added mac keybinding for backward-delete-char

--- lib/key-bindings.zsh
+++ lib/key-bindings.zsh
@@ -126,6 +126,7 @@ bindkey '\C-x\C-e' edit-command-line
 bindkey "^[m" copy-prev-shell-word
 
 # Make the delete key (or Fn + Delete on the Mac) work instead of outputting a ~
+bindkey '^?' backward-delete-char
 bindkey "^[[3~" delete-char
 bindkey "^[3;5~" delete-char
 bindkey "\e[3~" delete-char

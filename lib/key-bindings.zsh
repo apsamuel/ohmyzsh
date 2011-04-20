a9719d18577294c262d142d855ab3ab2f72fe87e
Arthur Kalmenson <arthur.kalm@gmail.com>
2011-04-19 22:40:46 -0400

    Added bindings for Gnome terminal.

--- lib/key-bindings.zsh
+++ lib/key-bindings.zsh
@@ -128,6 +128,7 @@ bindkey "^[m" copy-prev-shell-word
 # Make the delete key (or Fn + Delete on the Mac) work instead of outputting a ~
 bindkey "^[[3~" delete-char
 bindkey "^[3;5~" delete-char
+bindkey "\e[3~" delete-char
 
 # consider emacs keybindings:
 

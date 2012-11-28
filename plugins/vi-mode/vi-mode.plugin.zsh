90cf34eae7cce14f45b65ddba1588be83eecaa42
Max Bane <max.bane@gmail.com>
2012-11-28 02:02:58 -0500

    Revert "Fix to restore bindings after switching to vi-mode"
    This reverts commit 6c76fcc412b77da4b1e438ce8790a522ea36eeb0 -- this commit
    was a bad idea, because it makes vi-mode very difficult to use. The default
    `bindkey` keybindings are NOT MEANT to coexist with `bindkey -v` Vi mode;
    that's why `bindkey -v` clears them in the first place! Restoring all of the
    default keybindings after enabling Vi mode, the way the reverted commit did,
    causes many collisions between those default keybindings that begin with ESC
    and the command-mode-initiating ESC of Vi mode. See Issue 1438 of
    robbyrussell/oh-my-zsh. If people have custom keybindings, they should create
    them in their ~/.zshrc AFTER enabling the vi-mode plugin and sourcing
    oh-my-zsh.sh.
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    # On branch master
    # Changes to be committed:
    #   (use "git reset HEAD <file>..." to unstage)
    #
    #	modified:   plugins/vi-mode/vi-mode.plugin.zsh
    #

--- plugins/vi-mode/vi-mode.plugin.zsh
+++ plugins/vi-mode/vi-mode.plugin.zsh
@@ -95,8 +95,6 @@ function zle-line-finish() {
 zle -N zle-line-finish
 
 bindkey -v
-for bind in ${(@f)binds}; do eval $bind; done
-unset binds
 
 # allow vv to edit the command line (standard behaviour)
 autoload -Uz edit-command-line

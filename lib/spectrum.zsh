0a0a4caed12ac0211acfb3e2e197072b83a83947
LFDM <1986gh@gmail.com>
2013-12-31 14:54:34 +0100

    Updates spectrum.zsh

--- lib/spectrum.zsh
+++ lib/spectrum.zsh
@@ -19,6 +19,9 @@ for color in {000..255}; do
   BG[$color]="%{[48;5;${color}m%}"
 done
 
+
+ZSH_SPECTRUM_TEXT=${ZSH_SPECTRUM_TEXT:-Arma virumque cano Troiae qui primus ab oris}
+
 # Show all 256 colors with color number
 function spectrum_ls() {
   setopt localoptions nopromptsubst

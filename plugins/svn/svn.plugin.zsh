14b396e619e5a1be9bd5bf86957d809744195d8e
DanielFGray <DanielFGray@gmail.com>
2014-11-16 02:47:35 -0600

    ignore any grep aliases that might be defined

--- plugins/svn/svn.plugin.zsh
+++ plugins/svn/svn.plugin.zsh
@@ -91,7 +91,7 @@ svn_dirty_choose_pwd () {
 function svn_dirty_choose_pwd () {
   if in_svn; then
     root=$PWD
-    if $(svn status $root 2> /dev/null | grep -Eq '^\s*[ACDIM!?L]'); then
+    if $(svn status $root 2> /dev/null | command grep -Eq '^\s*[ACDIM!?L]'); then
       # Grep exits with 0 when "One or more lines were selected", return "dirty".
       echo $1
     else

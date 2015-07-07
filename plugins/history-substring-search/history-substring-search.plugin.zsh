e6737bfc83bd0b071d381341ccb6f654738c9280
Andrew Janke <andrew@apjanke.net>
2015-07-07 16:48:05 -0400

    history-substring-search: update to upstream version 2015-09-28
    Updates OMZ's copy to commit 2c295432175990c1bb4e90bc13f609daa67a25d6 from zsh-users/zsh-history-substring-search

--- plugins/history-substring-search/history-substring-search.plugin.zsh
+++ plugins/history-substring-search/history-substring-search.plugin.zsh
@@ -23,3 +23,14 @@ if [[ -n "$terminfo[kcud1]" ]]; then
   bindkey -M emacs "$terminfo[kcud1]" history-substring-search-down
   bindkey -M viins "$terminfo[kcud1]" history-substring-search-down
 fi
+
+
+# Bind terminal-specific up and down keys
+
+if [[ -n "$terminfo[kcuu1]" ]]; then
+  bindkey "$terminfo[kcuu1]" history-substring-search-up
+fi
+if [[ -n "$terminfo[kcud1]" ]]; then
+  bindkey "$terminfo[kcud1]" history-substring-search-down
+fi
+

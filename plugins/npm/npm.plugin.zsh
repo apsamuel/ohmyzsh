apsamuel/ohmyzsh

commit 62f45b6f1757248c46d5f2f18def23a9a895b529

npm: only load completion if npm exists

plugins/npm/npm.plugin.zsh
@@ -1,8 +1,10 @@
-__NPM_COMPLETION_FILE="${ZSH_CACHE_DIR}/npm_completion"
+(( $+commands[npm] )) && {
+    __NPM_COMPLETION_FILE="${ZSH_CACHE_DIR}/npm_completion"
 
-if [[ ! -f $__NPM_COMPLETION_FILE ]]; then
-    npm completion >! $__NPM_COMPLETION_FILE || rm -f $__NPM_COMPLETION_FILE
-fi
+    if [[ ! -f $__NPM_COMPLETION_FILE ]]; then
+        npm completion >! $__NPM_COMPLETION_FILE || rm -f $__NPM_COMPLETION_FILE
+    fi
+}
 
 source $__NPM_COMPLETION_FILE
 

commit bf42244cf177d7173b21c2897a9fa88fba8c5b66

Properly fix npm completion source
Problem introduced in 62f45b6.

plugins/npm/npm.plugin.zsh
@@ -4,9 +4,9 @@
     if [[ ! -f $__NPM_COMPLETION_FILE ]]; then
         npm completion >! $__NPM_COMPLETION_FILE || rm -f $__NPM_COMPLETION_FILE
     fi
-}
 
-source $__NPM_COMPLETION_FILE
+    source $__NPM_COMPLETION_FILE
+}
 
 # Install dependencies globally
 alias npmg="npm i -g "
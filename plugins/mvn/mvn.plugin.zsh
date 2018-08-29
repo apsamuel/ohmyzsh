c57ecee20abeba60c06ccbdf91b047b84c3dad3f
Janosch Schwalm <janosch.schwalm@gmail.com>
2018-08-29 21:00:06 +0200

    execute mvnw with "mvn" when mvnw-file is present (#6484)
    * executing mvnw, when mvnw-file exists
    indriectly enable autocompletion for mvnw
    
    * inform the user :)

--- plugins/mvn/mvn.plugin.zsh
+++ plugins/mvn/mvn.plugin.zsh
@@ -46,6 +46,9 @@ alias mvn="mvn-or-mvnw"
 # Run mvn against the pom found in a project's root directory (assumes a git repo)
 alias 'mvn!'='mvn -f $(git rev-parse --show-toplevel 2>/dev/null || echo ".")/pom.xml'
 
+# either use orignal mvn oder the mvn wrapper
+alias mvn="mvn-or-mvnw"
+
 # aliases
 alias mvnag='mvn archetype:generate'
 alias mvnboot='mvn spring-boot:run'

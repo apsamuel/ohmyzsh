7748c84611b657cb15e089996de63363c7b8122a
Yusuf Kocaman <yusuf.kocaman@enuygun.com>
2019-01-07 17:24:44 +0300

    added change namespace and rolling restart functions for kubectl

--- plugins/kubectl/kubectl.plugin.zsh
+++ plugins/kubectl/kubectl.plugin.zsh
@@ -100,6 +100,7 @@ alias kgns='kubectl get namespaces'
 alias kens='kubectl edit namespace'
 alias kdns='kubectl describe namespace'
 alias kdelns='kubectl delete namespace'
+alias kcn='kubectl config set-context $(kubectl config current-context) --namespace' #change namespace
 
 # ConfigMap management
 alias kgcm='kubectl get configmaps'

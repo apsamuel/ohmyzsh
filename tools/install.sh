d39e1d870af4c3f1d33e624d23786b4faa746937
Marc Cornellà <marc.cornella@live.com>
2019-05-28 17:34:05 +0200

    installer: change to --unattended argument and add docs

--- tools/install.sh
+++ tools/install.sh
@@ -23,6 +23,7 @@
 #   REPO    - name of the GitHub repo to install from (default: ohmyzsh/ohmyzsh)
 #   REMOTE  - full remote URL of the git repo to install (default: GitHub via HTTPS)
 #   BRANCH  - branch to check out immediately after install (default: master)
+#
 # Other options:
 #   CHSH    - 'no' means the installer will not change the default shell (default: yes)
 #   RUNZSH  - 'no' means the installer will not run zsh after the install (default: yes)

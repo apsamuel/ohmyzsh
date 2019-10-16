apsamuel/ohmyzsh

commit a3cd13ae2c5544a6991bb24d1164db8da02f801b

fzf: check for dpkg before checking for fzf in debian
Fixes #8253
Co-authored-by: Mariusz B <mb@binary-offensive.com>

plugins/fzf/fzf.plugin.zsh
@@ -1,9 +1,9 @@
 function setup_using_base_dir() {
-	# Declare all variables local not no mess with outside env in any way
-	local fzf_base
-	local fzf_shell
-	local fzfdirs
-	local dir
+    # Declare all variables local not no mess with outside env in any way
+    local fzf_base
+    local fzf_shell
+    local fzfdirs
+    local dir
 
     test -d "${FZF_BASE}" && fzf_base="${FZF_BASE}"
 
@@ -58,7 +58,7 @@ function setup_using_base_dir() {
 
 
 function setup_using_debian_package() {
-    dpkg -s fzf &> /dev/null
+    (( $+command[dpkg] )) && dpkg -s fzf &> /dev/null
     if (( $? )); then
         # Either not a debian based distro, or no fzf installed. In any case skip ahead
         return 1

commit 74c3cfb1eea156781904d8cac2034f6d6f0467df

fzf: fix regression (#8269)
Typo introduced in a3cd13a fixing #8253

plugins/fzf/fzf.plugin.zsh
@@ -58,7 +58,7 @@ function setup_using_base_dir() {
 
 
 function setup_using_debian_package() {
-    (( $+command[dpkg] )) && dpkg -s fzf &> /dev/null
+    (( $+commands[dpkg] )) && dpkg -s fzf &> /dev/null
     if (( $? )); then
         # Either not a debian based distro, or no fzf installed. In any case skip ahead
         return 1
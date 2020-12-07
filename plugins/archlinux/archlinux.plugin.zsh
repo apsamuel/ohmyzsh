4999d7da8e9a80da72418604136938a380fcea0c
ratijas <me@ratijas.tk>
2020-12-08 00:00:25 +0300

    refactor(archlinux): prevent leaking local variables (#9476)
    Co-authored-by: Marc Cornellà <marc.cornella@live.com>

--- plugins/archlinux/archlinux.plugin.zsh
+++ plugins/archlinux/archlinux.plugin.zsh
@@ -282,15 +282,14 @@ function upgrade() {
 }
 
 function pacmanallkeys() {
-  emulate -L zsh
   curl -s https://www.archlinux.org/people/{developers,trustedusers}/ | \
     awk -F\" '(/pgp.mit.edu/) { sub(/.*search=0x/,""); print $1}' | \
     xargs sudo pacman-key --recv-keys
 }
 
 function pacmansignkeys() {
-  emulate -L zsh
-  for key in $*; do
+  local key
+  for key in $@; do
     sudo pacman-key --recv-keys $key
     sudo pacman-key --lsign-key $key
     printf 'trust\n3\n' | sudo gpg --homedir /etc/pacman.d/gnupg \
@@ -300,13 +299,13 @@ function pacmansignkeys() {
 
 if (( $+commands[xdg-open] )); then
   function pacweb() {
-    pkg="$1"
-    infos="$(LANG=C pacman -Si "$pkg")"
+    local pkg="$1"
+    local infos="$(LANG=C pacman -Si "$pkg")"
     if [[ -z "$infos" ]]; then
       return
     fi
-    repo="$(grep -m 1 '^Repo' <<< "$infos" | grep -oP '[^ ]+$')"
-    arch="$(grep -m 1 '^Arch' <<< "$infos" | grep -oP '[^ ]+$')"
+    local repo="$(grep -m 1 '^Repo' <<< "$infos" | grep -oP '[^ ]+$')"
+    local arch="$(grep -m 1 '^Arch' <<< "$infos" | grep -oP '[^ ]+$')"
     xdg-open "https://www.archlinux.org/packages/$repo/$arch/$pkg/" &>/dev/null
   }
 fi

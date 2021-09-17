a29ac136dbddabddc87cd1beb516d0b881c37db2
Al-Amin Ahamed <34349365+mralaminahamed@users.noreply.github.com>
2021-09-18 03:31:56 +0600

    feat(archlinux): add cleanup aliases to `yaourt` and `yay` (#10184)

--- plugins/archlinux/archlinux.plugin.zsh
+++ plugins/archlinux/archlinux.plugin.zsh
@@ -404,6 +404,8 @@ if (( $+commands[yaourt] )); then
   alias yainsd='yaourt -S --asdeps'
   alias yamir='yaourt -Syy'
   alias yaupd="yaourt -Sy"
+  alias yaclun='yaourt -Yc'
+  alias yaclf='yaourt -Scc'
   alias upgrade='yaourt -Syu'
 fi
 
@@ -424,6 +426,8 @@ if (( $+commands[yay] )); then
   alias yainsd='yay -S --asdeps'
   alias yamir='yay -Syy'
   alias yaupd="yay -Sy"
+  alias yaclun='yay -Yc'
+  alias yaclf='yay -Scc'
   alias upgrade='yay -Syu'
 fi
 

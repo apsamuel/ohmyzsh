5404cc4efe5bbdfe580812bfd16d2bbc3d0af2f8
Marc Cornellà <hello@mcornella.com>
2021-09-22 11:58:23 +0200

    Revert "feat(archlinux): add cleanup aliases to `yaourt` and `yay` (#10184)"
    This reverts commit 26fa4051aa0f3ae826c7e80a90412cd1ecc6e3ff.

--- plugins/archlinux/archlinux.plugin.zsh
+++ plugins/archlinux/archlinux.plugin.zsh
@@ -404,8 +404,6 @@ if (( $+commands[yaourt] )); then
   alias yainsd='yaourt -S --asdeps'
   alias yamir='yaourt -Syy'
   alias yaupd="yaourt -Sy"
-  alias yaclun='yaourt -Yc'
-  alias yaclf='yaourt -Scc'
   alias upgrade='yaourt -Syu'
 fi
 
@@ -426,8 +424,6 @@ if (( $+commands[yay] )); then
   alias yainsd='yay -S --asdeps'
   alias yamir='yay -Syy'
   alias yaupd="yay -Sy"
-  alias yaclun='yay -Yc'
-  alias yaclf='yay -Scc'
   alias upgrade='yay -Syu'
 fi
 

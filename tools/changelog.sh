f3cbe0075f167b59fa49551ac2dc7fd4c61c2a6c
Marc Cornellà <hello@mcornella.com>
2021-10-26 18:26:02 +0200

    fix(changelog): go back to ignoring commits from merged branches

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -540,7 +540,7 @@ function main {
   local -a raw_fields
   for raw_commit in $raw_commits; do
     # Truncate list on versions with a lot of commits
-    if (( ++read_commits > 40 )); then
+    if [[ -z "$since" ]] && (( ++read_commits > 35 )); then
       truncate=1
       break
     fi

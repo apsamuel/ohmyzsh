737ebe53137c32ee3c021bee7a819afb51df0667
Marc Cornellà <hello@mcornella.com>
2021-12-28 22:09:11 +0100

    refactor(rvm): clean up `rb*` utilities definition and add more versions

--- plugins/rvm/rvm.plugin.zsh
+++ plugins/rvm/rvm.plugin.zsh
@@ -123,17 +123,12 @@ function gems {
 
 # TODO: Make this usable w/o rvm.
 function gems {
-	local current_ruby=`rvm-prompt i v p`
-	local current_gemset=`rvm-prompt g`
+  local current_ruby=`rvm-prompt i v p`
+  local current_gemset=`rvm-prompt g`
 
-	gem list $@ | sed -E \
-		-e "s/\([0-9, \.]+( .+)?\)/$fg[blue]&$reset_color/g" \
-		-e "s|$(echo $rvm_path)|$fg[magenta]\$rvm_path$reset_color|g" \
-		-e "s/$current_ruby@global/$fg[yellow]&$reset_color/g" \
-		-e "s/$current_ruby$current_gemset$/$fg[green]&$reset_color/g"
+  gem list $@ | sed -E \
+    -e "s/\([0-9, \.]+( .+)?\)/$fg[blue]&$reset_color/g" \
+    -e "s|$(echo $rvm_path)|$fg[magenta]\$rvm_path$reset_color|g" \
+    -e "s/$current_ruby@global/$fg[yellow]&$reset_color/g" \
+    -e "s/$current_ruby$current_gemset$/$fg[green]&$reset_color/g"
 }
-
-function _rvm_completion {
-  source $rvm_path"/scripts/zsh/Completion/_rvm"
-}
-compdef _rvm_completion rvm

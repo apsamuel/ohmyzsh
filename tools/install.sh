b6d1ede1894389ffa54547e3922885cd1146506f
Marc Cornellà <hello@mcornella.com>
2022-01-24 15:41:57 +0100

    fix(installer): define `$USER` if not defined
    Fixes missing $USER value in ArchLinux sh (bash)

--- tools/install.sh
+++ tools/install.sh
@@ -85,6 +85,9 @@ RUNZSH=${RUNZSH:-yes}
 KEEP_ZSHRC=${KEEP_ZSHRC:-no}
 OVERWRITE_CONFIRMATION=${OVERWRITE_CONFIRMATION:-yes}
 
+# Sane defaults
+USER=${USER:-$(whoami)}
+
 
 command_exists() {
   command -v "$@" >/dev/null 2>&1

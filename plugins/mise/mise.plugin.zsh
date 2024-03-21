3407dfe0ad7c850600bc40c0a1407537343e86a2
Ben Woodward <ben@benwoodward.me.uk>
2024-03-21 19:46:40 +0000

    fix(mise): update environment when loading the plugin (#12294)

--- plugins/mise/mise.plugin.zsh
+++ plugins/mise/mise.plugin.zsh
@@ -5,6 +5,9 @@ fi
 # Load mise hooks
 eval "$(mise activate zsh)"
 
+# Hook mise into current environment
+eval "$($__mise hook-env -s zsh)"
+
 # If the completion file doesn't exist yet, we need to autoload it and
 # bind it to `mise`. Otherwise, compinit will have already done that.
 if [[ ! -f "$ZSH_CACHE_DIR/completions/_mise" ]]; then

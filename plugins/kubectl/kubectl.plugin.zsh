44ba63730320239dbf4398357ce4f1fc74abebf0
Jérémy <PixiBixi@users.noreply.github.com>
2024-09-05 19:57:11 +0200

    feat(kubectl): show labels in aliases `kgpsl` (get pod) and `kgnosl` (get node) (#12612)
    Co-authored-by: Marc Cornellà <marc@mcornella.com>

--- plugins/kubectl/kubectl.plugin.zsh
+++ plugins/kubectl/kubectl.plugin.zsh
@@ -56,12 +56,6 @@ alias kdp='kubectl describe pods'
 alias kdelp='kubectl delete pods'
 alias kgpall='kubectl get pods --all-namespaces -o wide'
 
-# get pod by label: kgpl "app=myapp" -n myns
-alias kgpl='kgp -l'
-
-# get pod by namespace: kgpn kube-system"
-alias kgpn='kgp -n'
-
 # Service management.
 alias kgs='kubectl get svc'
 alias kgsa='kubectl get svc --all-namespaces'
